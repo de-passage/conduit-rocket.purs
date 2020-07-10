79a4d017be6d14fd2a55c7c46008c08ae45699fd
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2020-07-10 14:36:25 +0800

    Take config from environment

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use rocket::config::{Environment, Value};
+use std::collections::HashMap;
 use std::env;
 
 pub struct Config {
@@ -15,4 +17,35 @@ impl Config {
         })?;
         Ok(Config { secret })
     }
+}
+
+pub fn configure_rocket() -> Result<rocket::Config, String> {
+    let environment = Environment::active().map_err(|err| err.to_string())?;
+
+    let port = if let Ok(port_s) = env::var("PORT") {
+        port_s
+            .parse::<u16>()
+            .map_err(|err| format!("Port parsing failed: {}", err.to_string()))?
+    } else {
+        8000
+    };
+
+    let url = env::var("DATABASE_URL").map_err(|err|
+        format!("DATABASE_URL environment variable required, must point to a running Postgresql instance: {}", err.to_string()))?;
+    let mut database_config = HashMap::new();
+    database_config.insert("url", Value::from(url));
+
+    let mut databases = HashMap::new();
+    databases.insert("postgres", Value::from(database_config));
+
+    let mut c = rocket::Config::build(environment)
+        .port(port)
+        .extra("databases", databases);
+
+    if cfg!(release) {
+        c = c.secret_key(Config::from_env()?.secret)
+    }
+
+    c.finalize()
+        .map_err(|err| format!("Rocket configuration failed: {}", err.to_string()))
 }
