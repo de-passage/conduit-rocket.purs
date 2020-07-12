2b7cc28eed101da22efbb7afa674172474d7025a
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2020-07-12 13:41:21 +0800

    Delete users, triggers comments and article deletion

--- src/bin/clean-db.rs
+++ src/bin/clean-db.rs
@@ -1,3 +1,31 @@
+#[macro_use]
+extern crate diesel;
+
+#[path = "../schema.rs"]
+mod schema;
+
+use diesel::dsl::*;
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("OK!");
+    if cfg!(debug_assertions) {
+        dotenv().ok();
+    }
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let connection = PgConnection::establish(&database_url).expect(&format!(
+        "Database connection failed. Url: {}",
+        database_url
+    ));
+
+    {
+        use schema::users::dsl::*;
+        delete(users.filter(username.ne(all(vec!["Admin", "Sylvain Leclercq"]))))
+            .execute(&connection)
+            .expect("Deletion failed");
+    }
 }
